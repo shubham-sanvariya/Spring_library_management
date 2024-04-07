bad40edb1c96b7bc85d95f680373e34773491c0b
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-07 14:40:58 +0530

    adding book mapper

---
+++ src/main/java/com/shubh/library_management/mapper/BookMapper.java
@@ -0,0 +1,30 @@
+package com.shubh.library_management.mapper;
+
+import com.shubh.library_management.dto.BookDTO;
+import com.shubh.library_management.entity.Book;
+
+public class BookMapper {
+    public static Book mapToBook(BookDTO bookDTO){
+        Book book = new Book(
+            bookDTO.getBookId(),
+            bookDTO.getBookName(),
+            bookDTO.getBookAuthor(),
+            bookDTO.getBookCount(),
+            bookDTO.getBorrwedBy()
+        );
+
+        return book;
+    }
+
+    public static BookDTO mapToBookDTO(Book book){
+        BookDTO bookDTO = new BookDTO(
+            book.getBookId(),
+            book.getBookName(),
+            book.getBookAuthor(),
+            book.getBookCount(),
+            book.getBorrwedBy()
+        );
+
+        return bookDTO;
+    }
+}
