67d5e7f291148d63965018b68a367e6d1c129eef
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-17 17:21:54 +0530

    adding put mapping for returning book method

--- src/main/java/com/shubh/library_management/controller/BorrowedBookController.java
+++ src/main/java/com/shubh/library_management/controller/BorrowedBookController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -45,6 +47,16 @@ public class BorrowedBookController {
             }
             return responseEntity;
         } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+        }
+    }
+
+    @PutMapping("/{borrowedBookId}")
+    public ResponseEntity<?> returningBook(@PathVariable Long borrowedBookId){
+        try {
+            return new ResponseEntity<BorrowedBookDTO>(borrowedBookService.returnBook(borrowedBookId),
+            HttpStatus.OK);
+        } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
         }
     }
