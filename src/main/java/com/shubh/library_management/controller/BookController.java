49dd17d7b15b3c06283512be37930edd5cf4ce68
Shubham Sanwariya <shubhamsanvariyait@gmail.com>
2024-04-21 20:02:13 +0530

    adding mapping for updating book & deleting book

--- src/main/java/com/shubh/library_management/controller/BookController.java
+++ src/main/java/com/shubh/library_management/controller/BookController.java
@@ -4,13 +4,18 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.shubh.library_management.dto.BookDTO;
+import com.shubh.library_management.entity.Book;
+import com.shubh.library_management.mapper.BookMapper;
 import com.shubh.library_management.service.BookService;
 
 @RestController
@@ -30,5 +35,29 @@ public class BookController {
     @GetMapping
     public ResponseEntity<List<BookDTO>> getAllBooks(){
         return new ResponseEntity<List<BookDTO>>(bookService.getAllBooks(),HttpStatus.OK);
+    }
+
+    @PutMapping("/update-book")
+    public ResponseEntity<?> updateBook(@RequestBody BookDTO bookDTO){
+        try {
+            Book book = bookService.getBookbyId(bookDTO.getBookId());
+            int prevBookCount = bookDTO.getTotalBookCount() - book.getTotalBookCount();
+            bookDTO.setBookCount(prevBookCount + book.getBookCount());
+            Book updatedBook = BookMapper.mapToBook(bookDTO);
+            return new ResponseEntity<BookDTO>(bookService.updateBook(updatedBook),HttpStatus.CREATED);
+        }
+        catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @DeleteMapping("/{bookId}")
+    public ResponseEntity<String> deleteBookByBookId(@PathVariable Long bookId){
+        try {
+            bookService.deleteBook(bookId);
+            return new ResponseEntity<String>("Book deleted successfully", HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            return new ResponseEntity<String>(e.getMessage(),HttpStatus.NOT_FOUND);
+        }
     }
 }
